04e8289e64969c4bc638e40a1635d062172310b7
CrazyBoyM <ai-lab@foxmail.com>
2021-12-06 20:30:35 +0800

    add more judge api and add ctrl+s , ctrl + r to Stdin area

--- src/components/AreaStdIn.vue
+++ src/components/AreaStdIn.vue
@@ -1,15 +1,77 @@
 <script setup lang="ts">
 import { VAceEditor } from 'vue3-ace-editor'
 import { inject, Ref } from 'vue'
+import { useMessage } from 'naive-ui'
+import { setItem } from '/@/utils/localData'
+import { submitCode } from '/@/api/submission'
 
+const message = useMessage()
+
+const code = inject('code') as Ref<string>
 const input = inject('input') as Ref<string>
+const output = inject('output') as Ref<string>
+const lang = inject('lang') as Ref<string>
 const theme = inject('theme') as Ref<any>
+
+//  设置输出控制台内容的钩子
+const setOutput = inject('setOutput') as (val: string) => void
+
+//  代码评测状态
+const judging = inject('judging') as Ref<boolean>
+
+const handleSubmit = () => {
+  if(code.value.match(/^\s*$/)) {
+    message.warning('请输入代码')
+    return 0
+  }
+  judging.value = true
+  let data = {
+    code: code.value,
+    lang: lang.value,
+    input: input.value
+  }
+  submitCode(data, (output: string) => {
+    setOutput(output)
+    judging.value = false
+  })
+}
+
+//  处理保存事件
+const init = (editor: any) => {
+  let commands = editor.commands;
+  commands.addCommand({
+      name: "save",
+      bindKey: {win: "Ctrl-S", mac: "Command-S"},
+      exec: function(arg: any) {
+          setItem(
+              "code",
+              code.value
+          )
+          setItem(
+              "input",
+              input.value
+          )
+          setItem(
+              "output",
+              output.value
+          )
+          message.success('已保存')
+      }
+  })
+  commands.addCommand({
+    name: "run",
+      bindKey: {win: "Ctrl-R", mac: "Command-R"},
+      exec: function(arg: any) {
+          handleSubmit()
+      }
+  })
+}
 </script>
 
 <template>
   <v-ace-editor
     v-model:value="input"
-    @init=""
+    @init="init"
     :theme="theme.editorTheme"
     lang="text"
     placeholder='输入控制台'
--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,9 @@
-export const APP_Version = '1.0.1'
+export const APP_Version = '1.0.2'
 
-export const BASE_API_URL = 'http://guxin.ip3x.com:2358'
+const judge_urls = [
+  'http://guxin.ip3x.com:2358',
+  'http://zhenghang.ip3x.com:2358',
+  'http://guoshun.ip3x.com:2358',
+]
+
+export const BASE_API_URL = judge_urls[ Math.random()*judge_urls.length | 0]
