a05d3823a3940aa39dd026ad19e3f8b1ab270043
CrazyBoyM <ai-lab@foxmail.com>
2022-01-27 19:51:13 +0800

    修改图标

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,21 @@ import { resolve } from 'path'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { minifyHtml, injectHtml } from 'vite-plugin-html'
 
-const htmlPlugin = () => {
-  return {
-    name: 'html-transform',
-    transformIndexHtml(html) {
-      return html.replace(
-        /\/assets\//g,
-        'https://cdn.jsdelivr.net/gh/CrazyBoyM/haut-online-ide/dist/assets/'
-      )
-    }
-  }
-}
+// const htmlPlugin = () => {
+//   return {
+//     name: 'html-transform',
+//     transformIndexHtml(html) {
+//       return html.replace(
+//         /\/assets\//g,
+//         'https://cdn.jsdelivr.net/gh/CrazyBoyM/haut-online-ide/dist/assets/'
+//       )
+//     }
+//   }
+// }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), minifyHtml(), injectHtml(), htmlPlugin()],
+  plugins: [vue(), vueJsx(), minifyHtml(), injectHtml()],
   resolve: {
     alias: {
         "/@": resolve(__dirname, './src'), // 路径别名
