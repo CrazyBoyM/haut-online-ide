331b2883559fec8ac4594aba51f8ed0ea59ebec3
CrazyBoyM <ai-lab@foxmail.com>
2022-08-01 23:25:28 +0800

    fix: 修复vender.js打包过大的问题

--- vite.config.ts
+++ vite.config.ts
@@ -24,5 +24,38 @@ export default defineConfig({
         "/@": resolve(__dirname, './src'), // 路径别名
       },
     extensions: ['.ts', '.js', '.tsx', '.jsx'],
-  }
+  },
+  build: {
+    sourcemap: true,
+    outDir: 'distp', //指定输出路径
+    assetsDir: 'static/img/', // 指定生成静态资源的存放路径
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            const arr = id.toString().split('node_modules/')[1].split('/')
+            switch(arr[0]) {
+              case '@kangc':
+              case '@naturefw':
+              case '@popperjs':
+              case '@vue':
+              case 'axios':
+              case 'element-plus':
+                return '_' + arr[0]
+                break
+              default :
+                return '__vendor'
+                break
+            }
+          }
+        },
+        chunkFileNames: 'static/js1/[name]-[hash].js',
+        entryFileNames: 'static/js2/[name]-[hash].js',
+        assetFileNames: 'static/[ext]/[name]-[hash].[ext]'
+      },
+      // brotliSize: false, // 不统计
+      // target: 'esnext', 
+      // minify: 'esbuild' // 混淆器，terser构建后文件体积更小
+    }
+  },
 })
